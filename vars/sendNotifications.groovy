0e23791975580c7b47d8596fafc180256c0b44d8
infantraju-hex <85986544+infantraju-hex@users.noreply.github.com>
2021-08-27 11:33:24 +0530

    Update sendNotifications.groovy

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -1,33 +1,36 @@
-/**
- * Send notifications based on build status string
- */
-def call(String buildStatus = 'STARTED') {
-  // build status of null means successful
-  buildStatus = buildStatus ?: 'SUCCESS'
+def call(String recipients) {
+ def status, logRegex
 
-  // Default values
-  def colorName = 'RED'
-  def colorCode = '#FF0000'
-  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def summary = "${subject} (${env.BUILD_URL})"
-  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+    switch (currentBuild.currentResult) {
+        case 'SUCCESS':
+            status = 'successed'
+            logRegex = 'SUCCESS'
+            break
 
-  // Override default values based on build status
-  if (buildStatus == 'STARTED') {
-    color = 'YELLOW'
-    colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESS') {
-    color = 'GREEN'
-    colorCode = '#00FF00'
-  } else {
-    color = 'RED'
-    colorCode = '#FF0000'
-  }
+        case 'UNSTABLE':
+            status = 'unstable'
+            logRegex = 'FAILURE'
+            break
 
-  emailext (
-      to: 'infantraju123@gmail.com',
-      subject: subject,
-      body: details
-    )
+        case 'FAILURE':
+            status = 'failed'
+            logRegex = 'FAILURE'
+            break
+
+        case 'ABORTED':
+            status = 'canceled'
+            logRegex = 'ABORTED'
+            break
+
+    }
+    emailext(subject: "Build $status - ${JOB_NAME} #${BUILD_NUMBER} ",
+            body: """ <p> Build $status on Job:
+            <a style = "font-size:14px;text-decoration:underline;font-weight:bold" href="${BUILD_URL}/console">${
+                JOB_NAME
+            } - build# ${BUILD_NUMBER} </a></p>
+            <p> <pre> \${BUILD_LOG_REGEX, regex = "^.*?$logRegex.*?\$", linesBefore = 25, linesAfter = 150, maxMatches = 10, showTruncatedLines = false, escapeHtml = false} </pre></p> """
+            , mimeType: 'text/html'
+            , from: '"Jenkins server" <foo@acme.org>'
+            , to: "$recipients")
+
 }
